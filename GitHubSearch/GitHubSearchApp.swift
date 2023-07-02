bab7ebec13da1cf271656d9e60f838cd5b21e280
Ryuga <ribereo.minami@gmail.com>
2023-07-02 22:52:24 +0900

    Initial Commit

---
+++ GitHubSearch/GitHubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchApp.swift
+//  GitHubSearch
+//
+//  Created by Ryuga on 2023/07/02.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
