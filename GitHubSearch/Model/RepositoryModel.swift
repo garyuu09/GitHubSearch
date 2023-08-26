38864e808d7cdd9e99b225516e4707c838252d53
Ryuga <ribereo.minami@gmail.com>
2023-08-26 22:38:02 +0900

    feat: 取り出すデータに検索したレポジトリの画像を追加した。

--- GitHubSearch/Model/RepositoryModel.swift
+++ GitHubSearch/Model/RepositoryModel.swift
@@ -16,6 +16,15 @@ struct Repository: Codable, Identifiable {
 
 struct Owner: Codable {
     var login: String
+    var imageURL: URL
+
+    // Jsonでの名前とSwift上での名前の紐づけを行う。
+    enum CodingKeys: String, CodingKey {
+        case login
+        // 以下の形で対応付けができる。
+        // case (上記で宣言したSwift上の変数名) = "(Json上での変数名)"
+        case imageURL = "avatar_url"
+    }
 }
 
 struct RepositoryResponse: Codable {
