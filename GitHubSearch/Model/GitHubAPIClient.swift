92502b1fda24285f49d29a24c406316c0bc8b60a
Ryuga <ribereo.minami@gmail.com>
2023-10-03 21:59:05 +0900

    refactor: コードの可読性をあげるためにスペースを追加した。

--- GitHubSearch/Model/GitHubAPIClient.swift
+++ GitHubSearch/Model/GitHubAPIClient.swift
@@ -14,14 +14,17 @@ protocol GitHubAPIClientInterface {
 final class GitHubAPIClient: GitHubAPIClientInterface {
 
     func searchRepositories(url: URL) async throws -> RepositoryResponse {
+
         // URLRequestを作成
         var request = URLRequest(url: url)
         request.setValue("Bearer API_KEY", forHTTPHeaderField: "Authorization")
+
         // URLSessionでRequest
         let (data, response) = try await URLSession.shared.data(from: url)
         guard let httpResponse = response as? HTTPURLResponse else {
             throw APIError.unknownError
         }
+
         switch httpResponse.statusCode {
         case 200..<300:
             guard let decodedResponse = try?
